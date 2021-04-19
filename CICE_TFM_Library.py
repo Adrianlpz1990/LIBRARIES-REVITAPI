b47cddfbd3e9e6a3642b3b2f72f37c518c908686
Adrián López <adrian.lopezs.1990@gmail.com>
2021-04-19 21:18:26 +0200

    Start project

---
+++ CICE_TFM_Library.py
@@ -0,0 +1,103 @@
+#-*- coding: utf-8 -*-
+
+###COMMON LANGUAGE RUNTIME###	Es el entorno de ejecución que permite ejecutar código de varios lenguajes de programación distintos. Nos va a permitir ejecutar elementos que pertenecen a la API de Revit.
+import clr		
+
+###DOCUMENTMANAGER Y TRANSACTIONMANAGER###
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager			#Administracion del documento (Para localizar el documento)
+from RevitServices.Transactions import TransactionManager		#Puntos de guardado (Para modificar el documento)
+from System.Collections.Generic import *
+
+###Importar RevitAPI###
+clr.AddReference("RevitAPI")
+import Autodesk
+from Autodesk.Revit.DB import *
+import Autodesk.Revit.DB as DB
+from Autodesk.Revit.DB.Architecture import *
+from Autodesk.Revit.DB.Structure import *
+from Autodesk.Revit.DB.Plumbing import *
+from Autodesk.Revit.DB.Electrical import *
+from Autodesk.Revit.DB.Mechanical import *
+
+###Importar RevitAPI USER INTERFACE###
+clr.AddReference('RevitAPIUI')
+from Autodesk.Revit.UI import *
+from Autodesk.Revit.UI.Selection import *
+
+###GEOMETRIAS DE DYNAMO###
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import*
+
+###NODOS DYNAMO###
+clr.AddReference("RevitNodes")
+import Revit
+clr.ImportExtensions(Revit.Elements)			#Elementos espejo de Revit
+clr.ImportExtensions(Revit.GeometryConversion)	#Unidades
+
+###BIBLIOTECAS ESTANDAR DE IRONPYTHON###
+import sys
+sys.path.append(r'C:\Program Files (x86)\IronPython 2.7\Lib') 
+
+###.NET###		Espacios de nombres del sistema de la raíz .NET. Permite traer listas fuertemente tipadas, matrices, colecciones genéricas e Icollection, 
+import System		
+from System import Array	#Listas
+from System.Collections.Generic import *	#Listas fuertemente tipadas, etc
+
+###SISTEMA OPERATIVO###
+import os 
+
+###EXCEPCIONES(ERRORES)###		Guardar excepciones
+import traceback
+
+###DOCUMENTO ACTUAL E INTERFACE DE USUARIO###
+doc =  DocumentManager.Instance.CurrentDBDocument
+app =  DocumentManager.Instance.CurrentUIApplication.Application
+UIDocument =  DocumentManager.Instance.CurrentUIDocument
+uiapp = DocumentManager.Instance.CurrentUIApplication
+
+########################################################################################################################################################
+
+			#CONVERTIR UNIDADES#
+#VERSION HASTA 2021#
+def converToInt(param, valor):
+	"""Permite convertir las unidades del parametro de las de proyecto a internas"""
+	
+	UIunit = param.DisplayUnitsType
+	return UnitUtils.ConvertToInternalUnits(valor, UIunit)
+
+def projectUnits(x):
+	"""Lee el valor en unidades de proyecto"""
+
+	UIunit = Document.GetUnits(doc).GetFormatOptions(UnitType.UT_Length).DisplayUnits
+	return UnitUtils.ConvertFromInternalUnits(x, UIunit)
+
+#VERSIÓN 2021 EN ADELANTE#
+def converToInt2021(param, valor):
+	"""Permite convertir las unidades del parametro de las de proyecto a internas"""
+
+	UIunit = param.GetUnitTypeId()
+	return UnitUtils.ConvertToInternalUnits(valor, UIunit)
+
+#......................................................................................................
+
+def tolist(x):
+	"""Garantiza que el elemento sea iterable"""
+	if hasattr(x,'__iter__'): return x
+	else : return [x]
+
+#......................................................................................................
+
+def valorParametro(param):
+	"""Da el valor de los parámetros sin importar el tipo que sean"""
+	if param.StorageType == StorageType.String:
+		return param.AsString()
+	elif param.StorageType == StorageType.ElementId:
+		return doc.GetElement(param.AsElementId())
+	elif param.StorageType == StorageType.Double:
+		return param.AsDouble()
+	else:
+		return param.AsInteger()
+
+#......................................................................................................
